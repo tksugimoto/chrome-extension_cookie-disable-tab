f7b7674d2e6239c9fafa79b8f66d4deff3a4285a
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-10-03 12:19:06 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,44 @@
+﻿
+// インストール時に開いているすべてのタブにアイコン表示
+chrome.tabs.query({}, function (tabs) {
+	tabs.forEach(function (tab) {
+		chrome.pageAction.show(tab.id);
+	});
+});
+
+// タブが開いた/移動した時
+chrome.tabs.onUpdated.addListener(function (tabId) {
+	chrome.pageAction.show(tabId);
+	changePageActionInfo(tabId);
+});
+
+var isDisable = {};
+// 閉じた時
+chrome.tabs.onRemoved.addListener(function (tabId) {
+	delete isDisable[tabId];
+});
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+	var tabId = tab.id;
+	isDisable[tabId] = !isDisable[tabId];
+	changePageActionInfo(tabId);
+});
+
+function changePageActionInfo(tabId) {
+	chrome.pageAction.setIcon({
+		tabId: tabId,
+		path: "icon/icon_cookie_" + (isDisable[tabId] ? "disable" : "enable") + ".png"
+	});
+	chrome.pageAction.setTitle({
+		tabId: tabId,
+		title: "Cookie無効化: " + (isDisable[tabId] ? "ON" : "OFF")
+	});
+}
+
+setDisableCookieEvent({
+	urls: [
+		"*://*/*"
+	]
+}, function (tabId) {
+	return isDisable[tabId];
+});
---
+++ cookie_library.js
@@ -0,0 +1,55 @@
+﻿
+function setDisableCookieEvent(filter, isTarget) {
+	/*
+		filter: {
+			urls: [
+				// URL pattern
+				"http://example.com/*"
+			]
+		},
+		isTarget: function (tabId) {
+			return true;	// or false
+		}
+	*/
+	var callback_request = function (details) {
+		var requestHeaders = details.requestHeaders;
+		if (isTarget(details.tabId)) {
+			for (var i = 0; i < requestHeaders.length; ++i) {
+				if (requestHeaders[i].name === "Cookie") {
+					requestHeaders.splice(i, 1);
+					break;
+				}
+			}
+		}
+		return {
+			requestHeaders: requestHeaders
+		};
+	};
+	var opt_extraInfoSpec_request = [
+		"requestHeaders",
+		"blocking"
+	];
+	chrome.webRequest.onBeforeSendHeaders.addListener(callback_request, filter, opt_extraInfoSpec_request);
+	
+	var callback_response = function (details) {
+		var responseHeaders = details.responseHeaders;
+		if (isTarget(details.tabId)) {
+			var i = responseHeaders.length;
+			while (i > 0) {
+				i--;
+				if (responseHeaders[i].name === "Set-Cookie") {
+					// Set-Cookieヘッダは1つとは限らない
+					responseHeaders.splice(i, 1);
+				}
+			}
+		}
+		return {
+			responseHeaders: responseHeaders
+		};
+	};
+	var opt_extraInfoSpec_response = [
+		"responseHeaders",
+		"blocking"
+	];
+	chrome.webRequest.onHeadersReceived.addListener(callback_response, filter, opt_extraInfoSpec_response);
+}
